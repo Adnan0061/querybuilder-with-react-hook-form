7b8eee388882da6f22ac67efc775cbdd6b6c5957
Adnan Ahmed <adnan.ahmed0061@gmail.com>
2022-08-29 19:29:40 +0600

    context test

--- src/filterMachine.ts
+++ src/filterMachine.ts
@@ -1,56 +1,57 @@
-import { faker } from '@faker-js/faker';
-import { assign, createMachine } from 'xstate';
-import { Message } from './types';
+import { faker } from "@faker-js/faker";
+import { assign, createMachine } from "xstate";
+import { Message } from "./types";
 
 export const messages: Message[] = [
   {
     assignee: faker.internet.userName(),
     body: faker.lorem.paragraph(1),
-    inbox: faker.lorem.word(1),
-    status: [faker.datatype.number({ min: 0, max: 2 })],
+    inbox: faker.lorem.word(5),
+    status: [0],
     team: faker.commerce.department(),
   },
   {
     assignee: faker.internet.userName(),
     body: faker.lorem.paragraph(1),
-    inbox: faker.lorem.word(1),
-    status: [faker.datatype.number({ min: 0, max: 2 })],
+    inbox: faker.lorem.word(5),
+    status: [0],
     team: faker.commerce.department(),
   },
   {
     assignee: faker.internet.userName(),
     body: faker.lorem.paragraph(1),
-    inbox: faker.lorem.word(1),
-    status: [faker.datatype.number({ min: 0, max: 2 })],
+    inbox: faker.lorem.word(5),
+    status: [1],
     team: faker.commerce.department(),
   },
   {
     assignee: faker.internet.userName(),
     body: faker.lorem.paragraph(1),
-    inbox: faker.lorem.word(1),
-    status: [faker.datatype.number({ min: 0, max: 2 })],
+    inbox: faker.lorem.word(5),
+    status: [0, 1, 2],
     team: faker.commerce.department(),
   },
   {
     assignee: faker.internet.userName(),
     body: faker.lorem.paragraph(1),
-    inbox: faker.lorem.word(1),
-    status: [faker.datatype.number({ min: 0, max: 2 })],
+    inbox: faker.lorem.word(5),
+    status: [2],
     team: faker.commerce.department(),
   },
 ];
 
 export const filterMachine = createMachine(
   {
-    tsTypes: {} as import('./filterMachine.typegen').Typegen0,
+    tsTypes: {} as import("./filterMachine.typegen").Typegen0,
     context: {
       data: [],
+      filterType: "",
     },
     schema: {
       events: {} as
-        | { type: 'START' }
+        | { type: "START" }
         | {
-            type: 'FILTER';
+            type: "FILTER";
             status?: number[];
             assignee?: string;
             team?: string;
@@ -58,21 +59,22 @@ export const filterMachine = createMachine(
           },
       context: {} as {
         data: Message[];
+        filterType: string;
       },
     },
-    initial: 'idle',
+    initial: "idle",
     states: {
       idle: {
         on: {
           START: {
-            target: 'work',
+            target: "work",
           },
         },
       },
       work: {
         on: {
           FILTER: {
-            actions: 'filter',
+            actions: "filter",
           },
         },
       },
@@ -85,17 +87,19 @@ export const filterMachine = createMachine(
           return messages.filter((value) => {
             let output = true;
             if (ev.status) {
-              //TODO implement beter comparaison
-              output =
-                JSON.stringify(value.status) === JSON.stringify(ev.status);
+              //TODO implement better comparison
+              console.log(value.status, "status", ev.status);
+              output = JSON.stringify(value.status) === JSON.stringify(ev.status);
             }
             if (ev.assignee) {
               output = value.assignee === ev.assignee;
             }
             if (ev.team) {
+              console.log(value.team, "team", ev.team);
               output = value.team === ev.team;
             }
             if (ev.inbox) {
+              console.log(value.inbox, "sdafasd", ev.inbox);
               output = value.inbox === ev.inbox;
             }
             return output;
--- src/queryBuilderWithController.tsx
+++ src/queryBuilderWithController.tsx
@@ -1,25 +1,20 @@
-import { Controller, useFieldArray, useForm } from 'react-hook-form';
-import { ImBin } from 'react-icons/im';
-import Select from 'react-select';
-import { filterMachine } from './filterMachine';
-import {
-  assigneeOptionsList,
-  filterOptionsList,
-  inboxOptionsList,
-  operatorOptionsList,
-  statusOptionsList,
-  teamOptionsList,
-} from './options';
+import { Controller, useFieldArray, useForm } from "react-hook-form";
+import { ImBin } from "react-icons/im";
+import Select from "react-select";
+import { filterMachine } from "./filterMachine";
+import { assigneeOptionsList, filterOptionsList, inboxOptionsList, operatorOptionsList, statusOptionsList, teamOptionsList } from "./options";
 
-import { useMachine } from '@xstate/react';
-import { useEffect } from 'react';
+import { useMachine } from "@xstate/react";
+import { useEffect } from "react";
 
 const QueryBuilderWithController = () => {
   const [state, send] = useMachine(filterMachine);
 
   useEffect(() => {
-    send('START');
-  });
+    send("START");
+    console.log(state.value);
+    console.log(state.context.filterType);
+  }, [state.context.filterType]);
   const {
     control,
     handleSubmit,
@@ -29,19 +24,19 @@ const QueryBuilderWithController = () => {
     defaultValues: {
       filter: [
         {
-          filterOptions: { value: 'status', label: 'Status' },
-          operatorOptions: { value: '_eq', label: 'equal (=)' },
-          valueOptions: { value: [0, 1, 2], label: 'all' },
+          filterOptions: { value: "status", label: "Status" },
+          operatorOptions: { value: "_eq", label: "equal (=)" },
+          valueOptions: { value: [0, 1, 2], label: "all" },
         },
       ],
     },
-    mode: 'onBlur',
+    mode: "onBlur",
   });
 
-  const filterOptionsArray = watch('filter');
+  const filterOptionsArray = watch("filter");
 
   const { fields, append, remove } = useFieldArray({
-    name: 'filter',
+    name: "filter",
     control,
   });
 
@@ -54,141 +49,117 @@ const QueryBuilderWithController = () => {
       team?: string;
       inbox?: string;
     } = {
-      status: options === 'status' ? (value as number[]) : undefined,
-      assignee: options === 'agent_id' ? (value as string) : undefined,
-      inbox: options === 'inbox_id' ? (value as string) : undefined,
-      team: options === 'team_id' ? (value as string) : undefined,
+      status: options === "status" ? (value as number[]) : undefined,
+      assignee: options === "agent_id" ? (value as string) : undefined,
+      inbox: options === "inbox_id" ? (value as string) : undefined,
+      team: options === "team_id" ? (value as string) : undefined,
     };
 
-    send({ type: 'FILTER', ...filterQuery });
-    
+    send({ type: "FILTER", ...filterQuery });
   };
 
   return (
-    <div className='mx-auto my-9 p-5 w-2/3 rounded-lg bg-slate-100'>
-      <div className='flex justify-between items-center'>
-        <h3 className='text-third text-xl'>Create Filter</h3>
+    <div className="mx-auto my-9 p-5 w-2/3 rounded-lg bg-slate-100">
+      <div className="flex justify-between items-center">
+        <h3 className="text-third text-xl">Create Filter</h3>
         <div>
-          <span className='cursor-pointer float-right rounded-3xl hover:bg-sky-100 p-3 text-gray-500'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-5 w-5'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-            >
+          <span className="cursor-pointer float-right rounded-3xl hover:bg-sky-100 p-3 text-gray-500">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path
-                fillRule='evenodd'
-                d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-                clipRule='evenodd'
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
               />
             </svg>
           </span>
         </div>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className=''>
+      <form onSubmit={handleSubmit(onSubmit)} className="">
         {fields.map((item, index) => {
           // console.log(item);
           return (
-            <div className='divide-y divide-primary' key={item.id}>
-              <div className='flex justify-between items-center gap-3 mt-3 bg-slate-200 mr-5 mb-8 bg-secondary p-3 rounded-md'>
-                <Controller
+            <div className="divide-y divide-primary" key={item.id}>
+              <div className="flex justify-between items-center gap-3 mt-3 bg-slate-200 mr-5 mb-8 bg-secondary p-3 rounded-md">
+                {/* <Controller
                   name={`filter.${index}.filterOptions`}
                   control={control}
                   render={({ field }) => {
-                    return (
-                      <Select
-                        className='w-[26%]'
-                        {...field}
-                        options={filterOptionsList}
-                      />
-                    );
+                    return <Select className="w-[26%]" {...field} options={filterOptionsList} />;
                   }}
-                />
+                /> */}
 
-                <Controller
+                <Select className="w-[26%]" onChange={(e) => (state.context.filterType = e?.value)} options={filterOptionsList} />
+
+                {/* <Controller
                   name={`filter.${index}.operatorOptions`}
                   control={control}
                   render={({ field }) => {
-                    return (
-                      <Select
-                        className='w-[26%]'
-                        {...field}
-                        options={
-                          filterOptionsArray[index].filterOptions.value ===
-                          'status'
-                            ? operatorOptionsList('status')
-                            : filterOptionsArray[index].filterOptions
-                                .value === 'agent_id'
-                            ? operatorOptionsList('agent_id')
-                            : filterOptionsArray[index].filterOptions
-                                .value === 'inbox_id'
-                            ? operatorOptionsList('inbox_id')
-                            : filterOptionsArray[index].filterOptions
-                                .value === 'team_id'
-                            ? operatorOptionsList('team_id')
-                            : [{ value: 'null', label: 'null' }]
-                        }
-                      />
-                    );
-                  }}
+                    return ( */}
+                <Select
+                  className="w-[26%]"
+                  // {...field}
+                  options={
+                    filterOptionsArray[index].filterOptions.value === "status"
+                      ? operatorOptionsList("status")
+                      : filterOptionsArray[index].filterOptions.value === "agent_id"
+                      ? operatorOptionsList("agent_id")
+                      : filterOptionsArray[index].filterOptions.value === "inbox_id"
+                      ? operatorOptionsList("inbox_id")
+                      : filterOptionsArray[index].filterOptions.value === "team_id"
+                      ? operatorOptionsList("team_id")
+                      : [{ value: "null", label: "null" }]
+                  }
                 />
+                {/* );
+                  }}
+                /> */}
 
-                <Controller
+                {/* <Controller
                   name={`filter.${index}.valueOptions`}
                   control={control}
                   render={({ field }) => {
-                    return (
-                      <Select
-                        className='w-[40%]'
-                        {...field}
-                        options={
-                          filterOptionsArray[index].filterOptions.value ===
-                          'status'
-                            ? statusOptionsList
-                            : filterOptionsArray[index].filterOptions
-                                .value === 'agent_id'
-                            ? assigneeOptionsList
-                            : filterOptionsArray[index].filterOptions
-                                .value === 'inbox_id'
-                            ? inboxOptionsList
-                            : filterOptionsArray[index].filterOptions
-                                .value === 'team_id'
-                            ? teamOptionsList
-                            : [{ value: 'null', label: 'null' }]
-                        }
-                      />
-                    );
-                  }}
+                    return ( */}
+                <Select
+                  className="w-[40%]"
+                  // {...field}
+                  options={
+                    filterOptionsArray[index].filterOptions.value === "status"
+                      ? statusOptionsList
+                      : filterOptionsArray[index].filterOptions.value === "agent_id"
+                      ? assigneeOptionsList
+                      : filterOptionsArray[index].filterOptions.value === "inbox_id"
+                      ? inboxOptionsList
+                      : filterOptionsArray[index].filterOptions.value === "team_id"
+                      ? teamOptionsList
+                      : [{ value: "null", label: "null" }]
+                  }
                 />
+                {/* );
+                  }}
+                /> */}
 
-                <button
-                  onClick={() => remove(index)}
-                  className=' flex justify-center items-center -mr-7 w-8 h-8 bg-[#D44F68] shadow-layout shadow-[#D44F68]/70 rounded-full'
-                >
-                  <ImBin className='text-md text-white' />
+                <button onClick={() => remove(index)} className=" flex justify-center items-center -mr-7 w-8 h-8 bg-[#D44F68] shadow-layout shadow-[#D44F68]/70 rounded-full">
+                  <ImBin className="text-md text-white" />
                 </button>
               </div>
             </div>
           );
         })}
 
-        <div className='flex justify-end gap-3 mt-5'>
+        <div className="flex justify-end gap-3 mt-5">
           <button
             onClick={() =>
               append({
-                filterOptions: { value: 'status', label: 'Status' },
-                operatorOptions: { value: '_eq', label: 'equal (=)' },
-                valueOptions: { value: '', label: '' },
+                filterOptions: { value: "status", label: "Status" },
+                operatorOptions: { value: "_eq", label: "equal (=)" },
+                valueOptions: { value: "", label: "" },
               })
             }
-            className='px-4 py-2 bg-sky-500 text-white rounded-md'
+            className="px-4 py-2 bg-sky-500 text-white rounded-md"
           >
             Add Filter
           </button>
-          <button
-            type='submit'
-            className='text-primary bg-green-500 px-4 py-2 text-white rounded-md'
-          >
+          <button type="submit" className="text-primary bg-green-500 px-4 py-2 text-white rounded-md">
             Search
           </button>
         </div>
