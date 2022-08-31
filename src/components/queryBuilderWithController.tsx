6dad2f82be039c5b50d1cf3df525300ed01ac989
chlbri <bri_lvi@icloud.com>
2022-08-31 11:16:38 +0000

    fix(machine): session #4

--- src/components/queryBuilderWithController.tsx
+++ src/components/queryBuilderWithController.tsx
@@ -1,18 +1,19 @@
-import { useFieldArray, useForm } from "react-hook-form";
-import { ImBin } from "react-icons/im";
-import Select from "react-select";
-import { useMachine } from "@xstate/react";
-import { useEffect } from "react";
-import { filterMachine } from "../config/filterMachine";
-import { InputProps, Operators, Options } from "../config/types";
+import { useMachine } from '@xstate/react';
+import { useEffect } from 'react';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { ImBin } from 'react-icons/im';
+import Select from 'react-select';
+import { filterMachine } from '../config/filterMachine';
+import { InputProps, Operators, Options } from '../config/types';
 
 export const QueryBuilderWithController = () => {
   const [state, send] = useMachine(filterMachine);
 
   useEffect(() => {
-    send("START");
-    // console.log(state.value);
-    // console.log(state.context.filterType);
+    // send('START');
+    console.log(state.value);
+    // console.log(state.context.value);
+    console.log('results', '=>', state.context.results);
   });
 
   // console.log(state.value, state.value.filter_selected);
@@ -27,52 +28,44 @@ export const QueryBuilderWithController = () => {
     defaultValues: {
       filter: [
         {
-          filterOptions: { value: "status", label: "Status" },
-          operatorOptions: { value: "_eq", label: "equal (=)" },
-          valueOptions: { value: [0, 1, 2], label: "all" },
+          filterOptions: { value: 'status', label: 'Status' },
+          operatorOptions: { value: '_eq', label: 'equal (=)' },
+          valueOptions: { value: [0, 1, 2], label: 'all' },
         },
       ],
     },
-    mode: "onBlur",
+    mode: 'onBlur',
   });
 
   // const filterOptionsArray = watch("filter");
 
   const { fields, append, remove } = useFieldArray({
-    name: "filter",
+    name: 'filter',
     control,
   });
 
   const onSubmit = (data: ConversationFilter) => {
-    console.log(data);
-    // const options = data.filter[0].filterOptions.value;
-    // const value = data.filter[0].valueOptions.value;
-    // const filterQuery: {
-    //   status?: number[];
-    //   assignee?: string;
-    //   team?: string;
-    //   inbox?: string;
-    // } = {
-    //   status: options === "status" ? (value as number[]) : undefined,
-    //   assignee: options === "agent_id" ? (value as string) : undefined,
-    //   inbox: options === "inbox_id" ? (value as string) : undefined,
-    //   team: options === "team_id" ? (value as string) : undefined,
-    // };
-    send({ type: "SUBMIT" });
+    // console.log(data);
+    send({ type: 'SUBMIT' });
   };
   console.log(state.value);
 
   return (
-    <div className="mx-auto my-9 p-5 w-2/3 rounded-lg bg-slate-100">
-      <div className="flex justify-between items-center">
-        <h3 className="text-third text-xl">Create Filter</h3>
+    <div className='mx-auto my-9 p-5 w-2/3 rounded-lg bg-slate-100'>
+      <div className='flex justify-between items-center'>
+        <h3 className='text-third text-xl'>Create Filter</h3>
         <div>
-          <span className="cursor-pointer float-right rounded-3xl hover:bg-sky-100 p-3 text-gray-500">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <span className='cursor-pointer float-right rounded-3xl hover:bg-sky-100 p-3 text-gray-500'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+            >
               <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
+                fillRule='evenodd'
+                d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+                clipRule='evenodd'
               />
             </svg>
           </span>
@@ -82,8 +75,8 @@ export const QueryBuilderWithController = () => {
         {fields.map((item, index) => {
           // console.log(item);
           return (
-            <div className="divide-y divide-primary" key={item.id}>
-              <div className="flex justify-between items-center gap-3 mt-3 bg-slate-200 mr-5 mb-8 bg-secondary p-3 rounded-md">
+            <div className='divide-y divide-primary' key={item.id}>
+              <div className='flex justify-between items-center gap-3 mt-3 bg-slate-200 mr-5 mb-8 bg-secondary p-3 rounded-md'>
                 {/* <Controller
                   name={`filter.${index}.filterOptions`}
                   control={control}
@@ -92,7 +85,16 @@ export const QueryBuilderWithController = () => {
                   }}
                 /> */}
 
-                <Select className="w-[26%]" onChange={(e) => send({ type: "SELECT_OPTION", value: e?.value as Options })} options={state.context.options} />
+                <Select
+                  className='w-[26%]'
+                  onChange={(e) =>
+                    send({
+                      type: 'SELECT_OPTION',
+                      value: e?.value as Options,
+                    })
+                  }
+                  options={state.context.options}
+                />
 
                 {/* <Controller
                   name={`filter.${index}.operatorOptions`}
@@ -100,18 +102,45 @@ export const QueryBuilderWithController = () => {
                   render={({ field }) => {
                     return ( */}
                 <Select
-                  className="w-[26%]"
+                  className='w-[26%]'
                   // {...field}
                   // defaultValue={() => {
                   //   const a = operatorOptionsList("status");
                   //   return a[0];
                   // }}
-                  onChange={(e) => state.context.option && send({ type: "CHANGE_OPERATOR", value: e?.value as Operators })}
+                  onChange={(e) =>
+                    send({
+                      type: 'CHANGE_OPERATOR',
+                      value: e?.value as Operators,
+                    })
+                  }
                   options={
                     state.context.operators
                     //state.context.operators
                   }
                 />
+                {/* <select
+                  name=''
+                  id=''
+                  className='w-[26%]'
+                  onChange={(e) =>
+                    state.context.option &&
+                    send({
+                      type: 'CHANGE_OPERATOR',
+                      value: e?.currentTarget.value as Operators,
+                    })
+                  }
+                >
+                  {state.context.operators.map(({ label, value }, key) => {
+                    return (
+                      <option
+                        key={key}
+                        label={label}
+                        value={value.toLocaleString()}
+                      ></option>
+                    );
+                  })}
+                </select> */}
                 {/* );
                   }}
                 /> */}
@@ -122,43 +151,54 @@ export const QueryBuilderWithController = () => {
                   render={({ field }) => {
                     return ( */}
                 <Select
-                  className="w-[40%]"
+                  className='w-[40%]'
                   // {...field}
-                  onChange={(e) => send({ type: "CHANGE_VALUE", value: e?.value as InputProps["value"] })}
+                  onChange={(e) =>
+                    send({
+                      type: 'CHANGE_VALUE',
+                      value: e?.value as InputProps['value'],
+                    })
+                  }
                   options={state.context.values}
                 />
                 {/* );
                   }}
                 /> */}
 
-                <button onClick={() => remove(index)} className=" flex justify-center items-center -mr-7 w-8 h-8 bg-[#D44F68] shadow-layout shadow-[#D44F68]/70 rounded-full">
-                  <ImBin className="text-md text-white" />
+                <button
+                  onClick={() => remove(index)}
+                  className=' flex justify-center items-center -mr-7 w-8 h-8 bg-[#D44F68] shadow-layout shadow-[#D44F68]/70 rounded-full'
+                >
+                  <ImBin className='text-md text-white' />
                 </button>
               </div>
             </div>
           );
         })}
 
-        <div className="flex justify-end gap-3 mt-5">
+        <div className='flex justify-end gap-3 mt-5'>
           <button
             onClick={() =>
               append({
-                filterOptions: { value: "status", label: "Status" },
-                operatorOptions: { value: "_eq", label: "equal (=)" },
-                valueOptions: { value: "", label: "" },
+                filterOptions: { value: 'status', label: 'Status' },
+                operatorOptions: { value: '_eq', label: 'equal (=)' },
+                valueOptions: { value: '', label: '' },
               })
             }
-            className="px-4 py-2 bg-sky-500 text-white rounded-md"
+            className='px-4 py-2 bg-sky-500 text-white rounded-md'
           >
             Add Filter
           </button>
-          <button type="submit" className="text-primary bg-green-500 px-4 py-2 text-white rounded-md">
+          <button
+            type='submit'
+            className='text-primary bg-green-500 px-4 py-2 text-white rounded-md'
+          >
             Search
           </button>
         </div>
       </form>
       <form>
-        <select name="" id=""></select>
+        <select name='' id=''></select>
       </form>
       <div>{JSON.stringify(state.context.results)}</div>
     </div>
--- src/config/filterMachine.ts
+++ src/config/filterMachine.ts
@@ -1,103 +1,103 @@
-import { assign, createMachine } from "xstate";
-import { messages, options } from "./data";
-import { assignOperators, assignValues } from "./helpers";
-import type { InputProps, Message, Operators, Options } from "./types";
+import { assign, createMachine } from 'xstate';
+import { messages, options } from './data';
+import { assignOperators, assignValues } from './helpers';
+import type { InputProps, Message, Operators, Options } from './types';
 
 export const filterMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOlwgBswBiAZQBUBBAJXsVAAcB7WXAF1xd87EAA9EAJgAMAThIAOAGwSALAHZF8mWoDMKgKxKANCACeiAIyK5U21It7NmlTsUBfNybRY8hUlw4wACd0Pi4g2GoRbl4BIRFxBBcLEilVGX0VFWt9XLUTcwQLQxIdMr0JCXkdJSkdDy8MHAJiEgA3dAoAVzgopBAY-kFhfsTNKRJ9dKkNJUVZfLNLEvKKqpr5+s8Qb2a-EggwADN0Loo+Ps4eIfjRxBkJHRIZF-laq0ULC3kC5flS8oqSrVWpbRo+FqkADu4QA1tQAMIACUYADkAOIAUQA+gB5AAKmOYjHouOY0WucRGoESFgkuRIymy8kMukUag5vwQ8hUJGk1jULwkxSkimUDR2TV8rRhQXhyLRWOxADVGAAZACqmIpsWGCUsEjUE0NMh0r2F1nki0KKgsclWZSN+ik8ikKglu2l0LhdA1ACEALIASTY-UGVP1RXsihI3zZLOjaR+SwQqn0APKMiBQPZaQ9UshJFgXQARqh+IiURicQSiSSyTqbtSxPdHs9Xu8xV9k4UyhIM2VFPpFNldGp8xD9sWyxWFdWVeqtY2I3cEA8ni8ZG95h9u1zbfaHU6XW6J3tiHRMWrMQj6Hj8fQg7jUcu9augVyJNZUnZRWKqlMegeNs+BcIc8D9J6hbkFQr63DSlgOKkTJZtkYpKC8XJWE8zq2BYKg8nSWgOGeXokAEwShOEEFXLq8EtggOiyO2LyKDoVQcZU+hYWo-arNkehZPIhr6KRhYdN0cBwc2iSGCk8h0kCDzsjI8wSDxfHlI8XxJroonbFB+yHCcZx8NJkYKRM1hbtUyj6C83waQOAlZARhryGJ+yyrC5mrt26a2o8Lium6rift+v4uux+FWC4nmtNO5ZmWGlJvgh3IzKUygbMJ9j6GoKifkaqTVHoMxmipMjxUQvnpY5KbGqK2jjLotjKPYHnAUAA */
   createMachine(
     {
       context: {
-        option: "status",
-        operator: "_eq",
-        operators: assignOperators("status"),
-        values: assignValues("status"),
-        value: "",
+        option: 'status',
+        operator: '_eq',
+        operators: assignOperators('status'),
+        values: assignValues('status'),
+        value: '',
         options,
         results: messages,
       },
-      tsTypes: {} as import("./filterMachine.typegen").Typegen0,
+      tsTypes: {} as import('./filterMachine.typegen').Typegen0,
       schema: {
         context: {} as {
           option: Options;
           operator: Operators;
           operators: InputProps[];
           values: InputProps[];
-          value: InputProps["value"];
+          value: InputProps['value'];
           options: InputProps[];
           results: Message[];
         },
         events: {} as
-          | { type: "START" }
+          | { type: 'START' }
           | {
-              type: "SELECT_OPTION";
+              type: 'SELECT_OPTION';
               value: Options;
             }
           | {
-              type: "CHANGE_OPERATOR";
+              type: 'CHANGE_OPERATOR';
               value: Operators;
             }
           | {
-              type: "CHANGE_VALUE";
-              value: InputProps["value"];
+              type: 'CHANGE_VALUE';
+              value: InputProps['value'];
             }
           | {
-              type: "SUBMIT";
+              type: 'SUBMIT';
             },
       },
-      id: "(machine)",
-      initial: "idle",
+      id: '(machine)',
+      initial: 'idle',
       on: {
         SELECT_OPTION: {
-          actions: "setOption",
-          target: ".operators",
+          actions: 'setOption',
+          target: '.operators',
         },
       },
       states: {
         idle: {
-          always: "operators",
+          always: 'operators',
         },
         operators: {
           always: {
-            actions: "setOperators",
-            target: "values",
+            actions: 'setOperators',
+            target: 'values',
           },
         },
         values: {
           always: {
-            actions: "setValues",
-            target: "default",
+            actions: 'setValues',
+            target: 'default',
           },
         },
         default: {
           always: {
-            actions: "setDefaults",
-            target: "work",
+            actions: 'setDefaults',
+            target: 'work',
           },
         },
         work: {
           on: {
             CHANGE_OPERATOR: {
-              actions: "operator",
+              actions: 'operator',
             },
             CHANGE_VALUE: {
-              actions: "value",
+              actions: 'value',
             },
             SUBMIT: {
-              actions: "submit",
-              target: "submit",
+              actions: 'submit',
+              target: 'submit',
             },
           },
         },
         submit: {
           on: {
             CHANGE_OPERATOR: {
-              actions: "operator",
-              target: "work",
+              actions: 'operator',
+              target: 'work',
             },
             CHANGE_VALUE: {
-              actions: "value",
-              target: "work",
+              actions: 'value',
+              target: 'work',
             },
           },
         },
@@ -108,18 +108,23 @@ export const filterMachine =
         submit: assign({
           results: (ctx) => {
             return messages.filter((value) => {
-              let output = true;
+              // let output = true;
+              console.log('id', '=>', ctx.option);
+
               switch (ctx.option) {
-                case "status":
-                  output = JSON.stringify(value.status) === JSON.stringify(ctx.value);
-                case "agent_id":
-                  output = value.assignee === ctx.value;
-                case "team_id":
-                  output = value.team === ctx.value;
+                case 'status':
+                  return (
+                    JSON.stringify(value.status) ===
+                    JSON.stringify(ctx.value)
+                  );
+                case 'agent_id':
+                  return value.assignee === ctx.value;
+                case 'team_id':
+                  return value.team === ctx.value;
                 default:
-                  output = value.inbox === ctx.value;
+                  return value.inbox === ctx.value;
               }
-              return output;
+              // return output;
             });
           },
         }),
